d7b0ffdd8f226ffaa2029fdbbec9329af32e315e
David Weichselbaum <david.t.weichselbaum@gmail.com>
2022-08-08 20:52:06 +0200

    Add kotlin bindings file

---
+++ kotlin/bindings.py
@@ -0,0 +1,8 @@
+import sqlite3 as sl
+
+from ..utils.pairing import get_draft_autopairing as get_draft_autopairing_con
+
+
+def get_draft_autopairing(draft_player_numbers, player_ids, db_path):
+    con = sl.connect(db_path)
+    return get_draft_autopairing_con(draft_player_numbers, player_ids, con)
